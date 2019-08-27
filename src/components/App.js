cf87e7af500f5cb5e7e06371bce4216e8186cd4e
Marie Otaki <otakimarie@gmail.com>
2019-08-26 20:51:06 -0700

    modified files

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,8 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  async onSearchSubmit(term) {
+  state = { images: [] };
+  onSearchSubmit = async term => {
     const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
@@ -11,13 +12,15 @@ class App extends React.Component {
           "Client-ID 6cdc4f1d676458f8508f17781c3a3ad8349a6c5ecd6cd1e8ae60cb82ef212993"
       }
     });
-    console.log(response.data.results);
-  }
+
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
